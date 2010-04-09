78102a8f814ecec51bfece82c750a0efd5109aed
yohannes.marcell <yohannes.marcell@52855c48-c112-f495-7fb5-799a7752b5cc>
2010-04-09 01:10:10 +0000

    Moore done

---
+++ trunk/Moore.java
@@ -0,0 +1,48 @@
+/* Moore.java */
+
+import java.util.*;
+import java.io.*;
+
+//Marcell is driving
+public class Moore{
+
+     private ArrayList<String> states;
+     private alphabet inputAlpha;
+     private HashMap<Pair, String> trans;
+     private String startState;
+     private ArrayList<String> acceptingStates;
+     private String currentState;
+   
+   public Moore(ArrayList<String> s1, alphabet a1, HashMap<Pair, String> t1, String s2, 
+   		 ArrayList<String> s3){
+	states = s1;
+	inputAlpha = a1;
+	trans = t1;
+	startState = s2;
+	acceptingStates = s3;
+
+	currentState = startState;
+	}
+
+   public void IsAccepting(){
+       return acceptingStates.contains(currentState);
+   }
+   
+   public boolean step(char i){
+      if(inputAlpha.valid(i)) {
+      
+      Pair np = new Pair(currentState, i);
+      currentState = trans.get(np);
+      return true;
+     }
+     else{
+      return false;
+     }
+   }
+
+
+   public static void main(String[] args){
+   
+   }// end Main
+
+}//end Moore Class
