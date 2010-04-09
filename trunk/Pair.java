c39ca5f4acf2a6fc89618a1cb4a9cb6f1ecb08f9
yohannes.marcell <yohannes.marcell@52855c48-c112-f495-7fb5-799a7752b5cc>
2010-04-09 01:10:24 +0000

    Pair done

---
+++ trunk/Pair.java
@@ -0,0 +1,25 @@
+/* Pair.java */
+
+import java.util.*;
+import java.io.*;
+
+//Marcell is driving
+public class Pair{
+
+   private String first;
+   private char second;
+
+   public Pair(String a, char b){
+     first = a;
+     second = b;
+   }
+
+   public String getFirst(){
+    return first;
+   }
+
+   public String getSecond(){
+    return second;
+   }
+
+}// end Pair Class
