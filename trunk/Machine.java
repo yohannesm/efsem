147473ebe2513651433b66d43d89fd6d49aeb27d
davidkraftishihama <davidkraftishihama@52855c48-c112-f495-7fb5-799a7752b5cc>
2010-04-16 00:08:01 +0000

    Put mealy and moore into one Machine file

---
+++ trunk/Machine.java
@@ -0,0 +1,135 @@
+import java.util.*;
+import java.io.*;
+
+public abstract class Machine{
+     public static boolean verbose = false;
+     public static boolean warning = false;
+     public static boolean trap = false;
+}
+//David is driving
+public class Mealy extends Machine{
+
+     private ArrayList<String> states;
+     private alphabet inputAlpha;
+     private HashMap<Pair<String, Character>, Pair<String,String> > trans;
+     private String startState;
+     private String currentState;
+
+     private StringBuffer output;
+     
+   
+   public Mealy(ArrayList<String> s1, alphabet a1, HashMap<Pair<String, Character>, Pair<String, String> > t1, String s2){
+	states = s1;
+	inputAlpha = a1;
+	trans = t1;
+	startState = s2;
+
+	currentState = startState;
+        output = new StringBuffer();
+	}
+
+   
+   public boolean step(char i){
+      if(inputAlpha.valid(i)) {
+      Character getChar = new Character(i);
+
+      Pair<String, Character> np = new Pair<String, Character>(currentState, getChar);
+      Pair<String, String> out = trans.get(np);
+      currentState = out.getFirst();
+      output.append(out.getSecond() );
+      return true;
+     }
+     else{
+      output = null;
+      return false;
+     }
+   }
+
+   public void reset() {
+	currentState = startState;
+        output = new StringBuffer();
+   }
+
+   public String getOutput() {
+      return output.toString();
+   }
+
+   public String run(String input) {
+       for (int i = 0; i < input.length; i++) {
+	   step(input.charAt(i) );
+	}
+       return getOutput();
+    }
+   }
+
+   
+
+}//end Mealy Class
+
+//Marcell is driving
+public class Moore extends Machine{
+
+     private ArrayList<String> states;
+     private alphabet inputAlpha;
+     private HashMap<Pair<String, Character>, String> trans;
+     private String startState;
+     private ArrayList<String> acceptingStates;
+     private String currentState;
+     private ArrayList<String> finalStates;
+   
+   public Moore(ArrayList<String> s1, alphabet a1, HashMap<Pair<String, Character>, String> t1, String s2, 
+   		 ArrayList<String> s3){
+	states = s1;
+	inputAlpha = a1;
+	trans = t1;
+	startState = s2;
+	acceptingStates = s3;
+
+	currentState = startState;
+	}
+
+   public boolean IsAccepting(){
+       return acceptingStates.contains(currentState);
+   }
+
+   public boolean IsFinal() {
+       return finalStates.contains(currentState);
+   }
+   
+   public boolean step(char i){
+      if(inputAlpha.valid(i)) {
+ 
+      if (IsFinal() ) return true;
+
+      Character getChar = new Character(i);
+
+      Pair<String, Character> np = new Pair<String, Character>(currentState, getChar);
+      currentState = trans.get(np);
+      return true;
+     }
+     else{
+      return false;
+     }
+   }
+
+   public void reset() {
+	currentState = startState;
+   }
+
+   public String getOutput() {
+      if ( IsAccepting() ) {
+	return "ACCEPTING";
+      } else {
+	return "REJECTING";
+      }
+   }
+
+   public String run(String input) {
+       for (int i = 0; i < input.length; i++) {
+	   step(input.charAt(i) );
+	}
+       return getOutput();
+    }	
+   }
+
+}//end Moore Class
