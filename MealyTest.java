5ba4d381ee2145e2555afd879c58614fbc19972c
davidkraftishihama <davidkraftishihama@52855c48-c112-f495-7fb5-799a7752b5cc>
2010-04-22 21:56:59 +0000

    oops

---
+++ MealyTest.java
@@ -0,0 +1,9 @@
+import org.junit.* ;
+import static org.junit.Assert.*;
+
+public class MealyTest {
+    @Test
+    public void test1() {
+        assertEquals(3, 3);
+    }
+}
---
+++ MooreTest.java
@@ -0,0 +1,9 @@
+import org.junit.* ;
+import static org.junit.Assert.*;
+
+public class MooreTest {
+    @Test
+    public void test1() {
+        assertEquals(3, 3);
+    }
+}
